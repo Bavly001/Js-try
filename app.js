0d4b61d188efc00a21512a0573f7719e746b415a
Bavly001 <a_bavly@yahoo.com>
2022-02-27 20:45:30 +0200

    add txt

--- app.js
+++ app.js
@@ -13,21 +13,32 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     
+    const text_box=addForm.querySelector('input[type="text"]');
     const value = addForm.querySelector('input[type="text"]').value;
     addForm.querySelector('input[type="text"]').value='';
-    
-    //create element
-    const li = document.createElement('li');
-    const delBtn = document.createElement('button');
 
-    //add content
-    delBtn.textContent='delete';
-    delBtn.className = 'delete';
-    li.textContent=value;
+    //empty value
+    if (value == ''){
+        text_box.style.outline="1px solid red"
+        addForm.innerHTML += '<p id="error">Please, Add your book name</p>'
+    }
+    else{
+        text_box.style.outline="none";
+        const text_err = addForm.querySelector('#error');
+        text_err.style.display = "none";
+        //create element
+        const li = document.createElement('li');
+        const delBtn = document.createElement('button');
 
-    //append to document
-    li.appendChild(delBtn);
-    list.appendChild(li);
+        //add content
+        delBtn.textContent='delete';
+        delBtn.className = 'delete';
+        li.textContent=value;
+
+        //append to document
+        li.appendChild(delBtn);
+        list.appendChild(li);
+    }
 })
 
 //hide books
